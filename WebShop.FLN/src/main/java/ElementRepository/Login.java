1d83938347e080256a3e6c0f3df6772b30d3c617
Varshinijm <varshinijm99@gmail.com>
2024-04-04 23:07:47 +0530

    Element Repo Login and Register module completed

--- WebShop.FLN/src/main/java/ElementRepository/Login.java
+++ WebShop.FLN/src/main/java/ElementRepository/Login.java
@@ -1,5 +1,46 @@
 package ElementRepository;
 
-public class Login {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import GenericLibrary.BasePage;
+
+public class Login extends BasePage {
+
+	public Login(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+/*========Finding Login Page elements using FindBy annotations===========*/
+	
+	@FindBy(id = "Email")
+	private WebElement emailTextBox;
+	
+	@FindBy(id = "Password")
+	private WebElement passwordTextBox;
+	
+	@FindBy(id = "RememberMe")
+	private WebElement rememberMeCheckBox;
+	
+	@FindBy(xpath = "//input[@value='Log in']")
+	private WebElement loginButton;
+
+/*=========Giving Getters Methods for Located Elements=====================*/
+	public WebElement getEmailTextBox() {
+		return emailTextBox;
+	}
+
+	public WebElement getPasswordTextBox() {
+		return passwordTextBox;
+	}
+
+	public WebElement getRememberMeCheckBox() {
+		return rememberMeCheckBox;
+	}
+
+	public WebElement getLoginButton() {
+		return loginButton;
+	}
 
 }
