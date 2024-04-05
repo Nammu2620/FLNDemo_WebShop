a27a8d7ec0d1c23884d295c76331b5afef9cf67b
Renuka <Renuka@DESKTOP-GU16FJG>
2024-04-05 09:33:46 +0530

    Element Repo Renuka

--- WebShop.FLN/src/main/java/ElementRepository/LogOut.java
+++ WebShop.FLN/src/main/java/ElementRepository/LogOut.java
@@ -1,5 +1,42 @@
 package ElementRepository;
 
-public class LogOut {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
+import GenericLibrary.BasePage;
+
+public class LogOut extends BasePage{
+	
+	public LogOut(WebDriver driver) {
+		super(driver);
+	}
+	
+	@FindBy(linkText = "Computing and Internet")
+	private WebElement computingAndInternetBook;
+
+	public WebElement getComputingAndInternetBook() {
+		return computingAndInternetBook;
+	}
+	
+	@FindBy(xpath = "//input[contains(@id,'add-to-cart-button')]")
+	private WebElement addToCartButton;
+
+	public WebElement getAddToCartButton() {
+		return addToCartButton;
+	}
+	
+	@FindBy(xpath = "//input[@name='removefromcart']")
+	private WebElement RemoveCheckBox;
+	
+	public WebElement getRemoveCheckBox() {
+		return RemoveCheckBox;
+	}
+	
+	@FindBy(xpath = "//input[@name='updatecart']")
+	private WebElement updateShoppingCart;
+	
+	public WebElement getUpdateShoppingCart() {
+		return updateShoppingCart;
+	}
 }
